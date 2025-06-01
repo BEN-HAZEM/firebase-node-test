606d5574f589d514919552df87f642710cf2bbf5
BEN-HAZEM <benhazem.print@gmail.com>
2025-06-01 16:36:23 +0200

    fix: add express and correct port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from your Firebase-hosted Node.js app!\n');
-});
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+
+app.get('/', (req, res) => {
+  res.send('Firebase App Hosting works ✅');
+});
+
+app.listen(PORT, () => {
+  console.log(Server is listening on port ${PORT});
 });
